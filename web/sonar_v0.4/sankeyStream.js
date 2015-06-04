57fd31147b4cbcfa6f5770258066cf31461553fb
Fernando Cucchietti <fercook@gmail.com>
2015-06-04 10:39:00 +0200

    Some new files

---
+++ web/sonar_v0.4/sankeyStream.js
@@ -0,0 +1,259 @@
+/*
+
+node: {"layer", "row", "name" or whatever}
+link: {"source", "target", "value"}
+
+*/
+//TODO: FIX BUG ON SIZE[1]
+
+sankeyStream = function() {
+  var sankeyStream = {},
+      nodeWidth = 0.5,
+      nodeWidthpx = 10,
+      yScale = 100,
+      nodePadding = 4,
+      size = [1, 1],
+      nodes = [],
+      links = [],
+      layers = 1,
+      offset = "None",
+      outside = {"layer": -1,"row":-1,"name": "Outside"},
+      curvature = 0.5;
+
+  sankeyStream.nodeWidth = function(_) {
+    if (!arguments.length) return nodeWidthpx;
+    nodeWidth = +_;
+    return sankeyStream;
+  };
+    
+  sankeyStream.nodePadding = function(_) {
+    if (!arguments.length) return nodePadding;
+    nodePadding = +_;
+    return sankeyStream;
+  };
+
+  sankeyStream.nodes = function(_) {
+    if (!arguments.length) return nodes;
+    nodes = _;
+    return sankeyStream;
+  };
+
+  sankeyStream.links = function(_) {
+    if (!arguments.length) return links;
+    links = _;
+    return sankeyStream;
+  };
+
+  sankeyStream.size = function(_) {
+    if (!arguments.length) return size;
+    size = _;
+    return sankeyStream;
+  };
+
+  sankeyStream.offset = function(_) {
+    if (!arguments.length) return offset;
+    offset = _;
+    return sankeyStream;
+  };
+    
+  sankeyStream.layout = function() {
+    computeNodeLinks();
+    computeNodeValues();
+    computeInternalVariables();
+    computeNodeBreadths();
+    computeNodeDepths();
+    computeLinkDepths();
+    return sankeyStream;
+  };
+
+  sankeyStream.relayout = function() {
+    computeInternalVariables();
+    computeNodeBreadths();
+    computeNodeDepths();
+    computeLinkDepths();
+    return sankeyStream;
+  };
+
+   sankeyStream.curvature = function(_) {
+      if (!arguments.length) return curvature;
+      curvature = +_;
+      return sankeyStream;
+    };
+
+  sankeyStream.link = function() {
+      
+    function link(d) {
+      var x0 = d.source.x + d.source.dx,
+          x1 = d.target.x,
+          xi = d3.interpolateNumber(x0, x1),
+          x2 = xi(curvature),
+          x3 = xi(1 - curvature),
+          y0 = d.source.y + d.sy + d.dy / 2,
+          y1 = d.target.y + d.ty + d.dy / 2;
+      return "M" + x0 + "," + y0
+           + "C" + x2 + "," + y0
+           + " " + x3 + "," + y1
+           + " " + x1 + "," + y1;
+    }
+
+    return link;
+  };
+    
+//
+  function computeInternalVariables(){
+      layers = d3.max(nodes, function(d){return d.layer;});
+      nodeWidthpx=nodeWidth*(size[0]/layers);     
+  }
+    
+  // Populate the sourceLinks and targetLinks for each node.
+  // Also, if the source and target are not objects, assume they are indices.
+  function computeNodeLinks() {
+    nodes.forEach(function(node) {
+      node.sourceLinks = [];
+      node.targetLinks = [];
+    });
+    links.forEach(function(link) {
+      var source = link.source,
+          target = link.target;
+      if (typeof source === "number") {
+          if (source == -1) { source = link.source = outside;}
+          else {source = link.source = nodes[link.source]};
+      }
+      if (typeof target === "number") {
+          if (target == -1) { target = link.target = outside;}
+          else {target = link.target = nodes[link.target]};
+      }
+      source.sourceLinks.push(link);
+      target.targetLinks.push(link);
+    });
+  }
+
+  // Compute the value (size) of each node by summing the associated links.
+  function computeNodeValues() {
+    nodes.forEach(function(node) {
+      node.value = Math.max(
+        d3.sum(node.sourceLinks, value),
+        d3.sum(node.targetLinks, value)
+      );
+    });
+  }
+
+  // Nodes are equally spaced in x
+    function computeNodeBreadths(){
+        nodes.forEach(function(node){        // nodeWidth*(size[0]/layers); ???
+            node.x = (size[0]/layers)*node.layer; // node*(width - nodeWidth) / (x - 1)???
+            node.dx = nodeWidthpx;
+        });
+    }
+    
+ /*    
+  // Iteratively assign the breadth (x-position) for each node.
+  // Nodes are assigned the maximum breadth of incoming neighbors plus one;
+  // nodes with no incoming links are assigned breadth zero, while
+  // nodes with no outgoing links are assigned the maximum breadth.
+ 
+ function computeNodeBreadths() {
+    var remainingNodes = nodes,
+        nextNodes,
+        x = 0;
+
+    while (remainingNodes.length) {
+      nextNodes = [];
+      remainingNodes.forEach(function(node) {
+          
+        if (node.xPos)
+            node.x = node.xPos;
+        else
+            node.x = x;
+
+        node.dx = nodeWidth;
+        node.sourceLinks.forEach(function(link) {
+          nextNodes.push(link.target);
+        });
+      });
+      remainingNodes = nextNodes;
+      ++x;
+    }
+
+    //
+    moveSinksRight(x);
+    scaleNodeBreadths((width - nodeWidth) / (x - 1));
+  }
+*/
+
+  function computeNodeDepths() {
+    var nodesByBreadth = d3.nest()
+        .key(function(d) { return d.x; })
+        .sortKeys(d3.ascending)
+        .entries(nodes)
+        .map(function(d) { return d.values; });
+      
+    var maxValueSum = d3.max(nodesByBreadth, function(nodesInLayer){ 
+          return d3.sum(nodesInLayer, function(d){return d.value});
+      });
+      console.log(maxValueSum+" people");
+    var scale = d3.scale.linear()
+        .range([0,size[1]-(nodesByBreadth[1].length - 1) * nodePadding])
+        .domain([0,maxValueSum]);
+      console.log(scale(maxValueSum));
+    var offsetPx = 0;
+    if (offset == "Top" ) {offsetPx = 0;}      
+      
+    nodesByBreadth.forEach(function(nodes) {
+        if (offset == "Bottom" ) {
+            offsetPx = Math.floor( size[1]-(scale(d3.sum(nodes, function(d){return d.value}))) ); 
+        }    
+        if (offset == "Centered" ) {
+            offsetPx = Math.floor( 0.5* (size[1]-scale(d3.sum(nodes, function(d){return d.value}))) ); 
+        }
+        prevY = offsetPx;
+        nodes.forEach(function(node, i) {
+          node.y = prevY;
+          var dy = scale(node.value);
+          node.dy = dy;
+          prevY = prevY + dy + nodePadding;
+        });
+      });
+
+      links.forEach(function(link) {
+        link.dy = scale(link.value);
+      });
+            
+    }
+
+  function computeLinkDepths() {
+    nodes.forEach(function(node) {
+      node.sourceLinks.sort(ascendingTargetDepth);
+      node.targetLinks.sort(ascendingSourceDepth);
+    });
+    nodes.forEach(function(node) {
+      var sy = 0, ty = 0;
+      node.sourceLinks.forEach(function(link) {
+        link.sy = sy;
+        sy += link.dy;
+      });
+      node.targetLinks.forEach(function(link) {
+        link.ty = ty;
+        ty += link.dy;
+      });
+    });
+
+    function ascendingSourceDepth(a, b) {
+      return a.source.y - b.source.y;
+    }
+
+    function ascendingTargetDepth(a, b) {
+      return a.target.y - b.target.y;
+    }
+  }
+
+  function center(node) {
+    return node.y + node.dy / 2;
+  }
+
+  function value(link) {
+    return link.value;
+  }
+
+  return sankeyStream;
+};
---
+++ web/sonar_v0.4/sonarSankey.js
@@ -0,0 +1,241 @@
+///////////////////////////
+/*
+var Room = (function(){
+        var timeslots = 0, rooms=[], acc_rooms=[];
+        function idx(){
+            
+        }
+        room.timeslots = function(x){
+          if (!arguments.length) return timeslots;
+          timeslots = x;
+          return room;            
+        };
+        room.rooms = function(x){
+          if (!arguments.length) return rooms;
+          if (arguments.length==1) return rooms[x]; //rooms at a given time
+          //timeslots = x;
+          return room;            
+        };
+        
+                    ins_timeslots: 0,
+                    ins_rooms: [],
+                    acc_rooms: [],
+                    idx: function(time, room){
+                        return this.acc_rooms[time]+room;},
+                    rooms: function(){return this.ins_rooms;}
+                    timeslots: function()
+                }
+            }
+             )();
+
+
+        "time_end": "2015-05-28 20:02:34",
+        "time_start": "2015-05-28 19:56:58",
+
+
+*/
+///////////////
+
+GraphParameters = {
+    "graphWidth": 4000,
+    "svgHeight": 500,
+    "graphHeight": 500,
+    "nodeWidth": 0.4,
+    "nodePadding": 50,
+    "offset": "Top",
+    "curvature": 0.5
+}
+
+var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S");
+
+function addMinutes(date, minutes) {
+    return new Date(date.getTime() + minutes*60000);
+}
+
+analysis = {links:null, nodes:null};
+
+var DATESTART=parseDate.parse("2015-05-28 20:00:00");
+var TOTALTIMESLOTS=14;
+var TIMEWINDOW = 15;
+var timeWindows = [];
+startdate = DATESTART;
+for (var n=0;n<TOTALTIMESLOTS;n++){
+    enddate = addMinutes(startdate, TIMEWINDOW);
+    timeWindows.push({start:startdate,end:enddate,toprocess:[]});
+    startdate=enddate;
+}
+
+queue()
+    .defer(d3.json, "graph.json") //"../DATA/prob_separated.json")
+.await(ready);
+
+function ready(error, data) {
+    //need to process data here
+    var timeslots = data.graph.length;
+    data.graph.forEach(function(slot) {
+        var slotstart = parseDate(slot["time_start"]);
+        var slotend = slot["time_end"];
+        for (var n=0;n<TOTALTIMESLOTS;n++){
+            if ( (timeWindows[n].start<=slotstart && slotend<=timeWindows[n].end) || // Clear case inside the period
+               (timeWindows[n].start<=slotstart && timeWindows[n].end<= slotend) ) // Straddle the break between periods, arbitrary choice
+              { timeWindows[n].toprocess.push(slot); }
+        }
+    });
+    //analysis = data; // HERE GOES THE ANALYSIS OF THE DATA
+    draw();
+}
+
+function draw() {
+
+    d3.selectAll("svg").remove();
+
+    var width = $("#svg").width(),
+        m_width = GraphParameters.graphWidth,
+        m_height = GraphParameters.graphHeight,
+        height = GraphParameters.svgHeight,
+        nodePadding = GraphParameters.nodePadding,
+        nodeWidth = GraphParameters.nodeWidth,
+        curvature = GraphParameters.curvature,
+        offset = GraphParameters.offset;
+
+    svg = d3.select("#svg").append("svg")
+        .attr("preserveAspectRatio", "xMidYMid")
+        .attr("viewBox", "0 0 " + GraphParameters.graphWidth + " " + height)
+        .attr("width", GraphParameters.graphWidth)
+        .attr("height", height);
+
+    svg.append("rect")
+        .attr("class", "sea")
+        .attr("width", GraphParameters.graphWidth)
+        .attr("height", height)
+        .style("fill", "white");
+    //    .on("click", click);
+
+    var maing = svg.append("g");
+
+    sankey = sankeyStream()
+        .nodeWidth(nodeWidth)
+        .curvature(curvature)
+        .nodePadding(nodePadding)
+        .size([GraphParameters.graphWidth, m_height])
+        .offset(offset);
+
+    path = sankey.link();
+
+    colors = d3.scale.category10();
+
+    sankey
+        .nodes(analysis.nodes)
+        .links(analysis.links)
+        .layout();
+    
+    mainplot = drawComponents(analysis);
+}
+
+function drawComponents(graph){
+    
+    var link = svg.append("g").selectAll(".link")
+        .data(graph.links).enter();
+//    console.log("Links");
+//    console.log(graph.links);
+    link.append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", function (d) {
+            return Math.max(0, d.dy);
+        })
+        .style("stroke", function (d) {
+            if (d.source.room == 4 || d.target.room == 4) {
+                return colors(d.source.room);
+            } else {
+                return "#000"
+            }
+        })
+        .sort(function (a, b) {
+            return b.dy - a.dy;
+        });
+    /*    link.append("title")
+        .text(function (d) {
+            return d.source.name + " → " + d.target.name + "\n" + format(d.value);
+        });
+        */
+
+    var node = svg.append("g").selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    /*        .call(d3.behavior.drag()
+            .origin(function (d) {
+                return d;
+            })
+            .on("dragstart", function () {
+                this.parentNode.appendChild(this);
+            })
+            .on("drag", dragmove));*/
+
+    node.append("rect")
+        .attr("height", function (d) {
+            return d.dy;
+        })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function (d) {
+            return d.color = colors(d.room);
+        })
+        .on("mouseover", function (d) {       
+            highlight = drawHighlight(d);
+        })
+        .on("mouseout", function (d) {
+            d3.selectAll(".highlightLink").remove();
+        });
+
+}
+
+
+function drawHighlight(highlightNode){
+    graph = {}
+    graph.links = highlightNode.sourceLinks.concat(highlightNode.targetLinks);
+//        .filter(function(d){return d.value > 0 && d.sy>0 && d.ty >0 ;});
+//    console.log("highlight:");
+//    console.log(graph.links);
+    //graph.nodes = [highlightNode];
+    
+        //console.log(graph.links.filter(function(d){return d.value > 0;}));
+    var link = svg.append("g").selectAll(".highlightLink")
+        .data(graph.links).enter();
+
+    link.append("path")
+        .attr("class", "highlightLink")
+        .attr("d", path)
+        .style("stroke-width", function (d) {
+            return Math.max(0, d.dy);
+        })
+        .style("fill", "none")
+        .style("stroke", function (d) {
+                return colors(d.source.room);
+        })
+        .sort(function (a, b) {
+            return b.dy - a.dy;
+        });
+/*
+    var node = svg.append("g").selectAll(".highlightNode")
+        .data(graph.nodes)
+        .enter().append("g")
+        .attr("class", "highlightNode")
+        .attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+
+    node.append("rect")
+        .attr("height", function (d) {
+            return d.dy;
+        })
+        .attr("width", sankey.nodeWidth())
+        .style("fill", function (d) {
+            return d.color = colors(d.room);
+        });
+*/
+    return link;
+}
